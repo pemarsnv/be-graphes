f4864704305b155f5fdb6bd823d258a5564129b1
Enzo Marietti <enzomarietti00@gmail.com>
2025-05-26 15:09:59 +0200

    AStar finished and tested

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,11 +1,26 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Path;
 
 public class AStarAlgorithm extends DijkstraAlgorithm {
 	
+
 	private LabelStar nodeToTab;
+	protected ShortestPathData data;
+	private Graph graph;
+	private int nbNodes;
+	protected int destination;
+	
+	protected LabelStar[] labels;
+	protected BinaryHeap<Label> heap;
 
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
@@ -18,7 +33,7 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
         for (int i = 0; i < nb; i++) {
             labels[i] = null;
         }
-        this.heap = new BinaryHeap <Label>();
+        this.heap = new BinaryHeap<Label>();
         
         nodeToTab = new LabelStar(data.getOrigin(), false, 0, null, data.getOrigin());
         this.insertLabel(nodeToTab);
@@ -32,21 +47,82 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
     }
     
     public void treatArc(Arc arc) {
-    	
-    	nodeToTab = new LabelStar(arc.getDestination(), false, labels[arc.getOrigin().getId()].getCoutRealise() + arc.getLength(), arc, arc.getOrigin());
-        int sommetCourant = nodeToTab.getSommetCourant();
-            
+        int sommetCourant = arc.getDestination().getId();
         if (labels[sommetCourant] == null) {
+            this.nodeToTab = new LabelStar(arc.getDestination(), false, labels[arc.getOrigin().getId()].getCoutRealise() + arc.getLength(), arc, data.getDestination());
          	notifyNodeReached(data.getGraph().get(sommetCourant));
             labels[sommetCourant] = nodeToTab;
             heap.insert(nodeToTab);
-       }
-            
-        if (labels[sommetCourant].getCoutRealise() > nodeToTab.getCoutRealise() && !labels[sommetCourant].getMarque()) {
-          	labels[sommetCourant].setCoutRealise(nodeToTab.getCoutRealise());
+       } 
+        else if (labels[sommetCourant].getCoutRealise() > labels[arc.getOrigin().getId()].getCoutRealise() + arc.getLength()) {
+          	labels[sommetCourant].setCoutRealise(labels[arc.getOrigin().getId()].getCoutRealise() + arc.getLength());
             labels[sommetCourant].setPere(arc);
         }
     	
+    	
     }
 
+    
+    @Override
+    protected ShortestPathSolution doRun() { // probleme au remove de la heap, peut etre refaire entierement l'algo
+
+        //On récupère les données de l'input
+        this.data = getInputData();
+        this.graph = data.getGraph();
+        this.nbNodes = graph.size();
+ 
+        notifyOriginProcessed(data.getOrigin());
+        
+        //On crée un tableau et un BinaryHeap pour stocker les labels
+        this.initiliazeLabels(this.nbNodes);
+        
+        //On prend et on traite la racine du tas 
+        Label currentLabel;
+
+        //Tant qu'il nous reste des labels à traiter et qu'on a pas atteint et marqué la destination...
+        do {
+            // on prend le sommet de plus petite valeur dans le tas, donc la racine du tas
+            // on le marque comme traité
+            currentLabel = heap.deleteMin();
+			currentLabel.setMarque(true);
+            labels[currentLabel.getSommetCourant()].setMarque(true);
+            //On notifie le sommet traité
+			notifyNodeMarked(data.getGraph().get(currentLabel.getSommetCourant()));
+
+        	//On va parcourir tous les arcs
+            for (Arc arc : graph.get(currentLabel.getSommetCourant()).getSuccessors()) {
+            	if (data.isAllowed(arc)) this.treatArc(arc);
+            }         
+        } while (!heap.isEmpty() && (labels[destination] == null || !labels[destination].getMarque()));
+
+        //On regarde si le sommet destination est atteint
+        destination = data.getDestination().getId();
+        Status status = Status.OPTIMAL;
+        Arc currentArc;
+        List<Arc> path = new ArrayList<Arc>();
+        
+        if (labels[destination] == null) {
+        	status = Status.INFEASIBLE;
+        } else {
+        	
+        	notifyDestinationReached(data.getGraph().get(destination));
+        	
+        	currentArc = labels[destination].getPere();
+
+            while (currentArc != null) {
+                path.add(currentArc);
+                currentArc = labels[currentArc.getOrigin().getId()].getPere();
+            }
+
+            //reverse the path
+            Collections.reverse(path);
+            
+        }
+        
+        // variable that will contain the solution of the shortest path problem
+        ShortestPathSolution solution = new ShortestPathSolution(data, status, new Path(graph, path));
+
+        // when the algorithm terminates, return the solution that has been found
+        return solution;
+    }
 }
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
@@ -4,7 +4,7 @@ import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Point;
 
-public class LabelStar extends Label {
+public class LabelStar extends Label {	
 	
 	private Node destination;
 
@@ -22,4 +22,14 @@ public class LabelStar extends Label {
 		return super.getCoutRealise() + this.calculateDistanceDestination();
 	}
 
+	@Override
+	public int compareTo(Label autre) {
+		if (this.getCoutTotal() < autre.getCoutTotal()) {
+			return -1;
+		} else if (this.getCoutTotal() > autre.getCoutTotal()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
